5d7eabf005e4e4f29fd0d0be43f97e29b1e48655
김현지 (Evelyn.Kim) <eve0204eve@gmail.com>
2025-11-25 21:22:15 +0900

    🔧 config: enhance webpack setup for multipage and aliases
    - Updated webpack config to support multiple HTML entry points
    - Added path aliases for cleaner and more maintainable imports
    - Prepared build structure for scalable multi-page publishing

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,43 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+// 페이지 목록 정의
+const pages = ['home', 'adopt', 'auth', 'sub'];
+
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  
+  // 멀티 엔트리 포인트 설정
+  entry: pages.reduce((entries, page) => {
+    entries[page] = [
+      // 공통 엔트리
+      './src/index.js', 
+      // 페이지별 엔트리
+      `./src/js/pages/${page}.js` 
+    ];
+    return entries;
+  }, {
+    // 메인 엔트리
+    main: './src/index.js'  
+  }),
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.[contenthash].js',
-    // 기본 이미지 출력 경로 역할
-    assetModuleFilename: 'assets/[name][ext]', 
+    // [name]으로 변경
+    filename: 'js/[name].[contenthash].js',  
+    assetModuleFilename: 'assets/[name][ext]',
+    clean: true
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), 
+      '@scss': path.resolve(__dirname, 'src/scss'), 
+      '@js': path.resolve(__dirname, 'src/js'), 
+      '@assets': path.resolve(__dirname, 'src/assets'), 
+    },
+    // 확장자 자동 해결
+    extensions: ['.js', '.jsx', '.scss', '.css']
   },
 
   module: {
@@ -22,17 +50,32 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          'sass-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                includePaths: [path.resolve(__dirname, 'src/scss')]
+              }
+            }
+          }
         ],
       },
 
       // 이미지
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
         type: 'asset/resource',
         generator: {
-          // dist/assets/로 복사 역할
-          filename: 'assets/[name][ext]', 
+          filename: 'assets/images/[name][ext]',
+        },
+      },
+
+      // 폰트
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext]',
         },
       },
     ],
@@ -41,14 +84,28 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
 
-    // CSS 파일 추출 플러그인 (한 번만!)
+    // CSS 파일 추출 플러그인
     new MiniCssExtractPlugin({
-      filename: 'css/styles.[contenthash].css',
+      // [name]으로 변경
+      filename: 'css/[name].[contenthash].css',  
     }),
 
-    // HTML 템플릿
+    // 각 페이지별 HtmlWebpackPlugin 자동 생성
+    ...pages.map(page => 
+      new HtmlWebpackPlugin({
+        template: `./src/pages/${page}/index.html`,
+        filename: `${page}.html`,
+        chunks: ['main', page], 
+        inject: 'body',
+      })
+    ),
+
+    // 메인 index.html (홈을 기본으로)
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './src/pages/home/index.html',
+      filename: 'index.html',
+      chunks: ['main', 'home'],
+      inject: 'body',
     }),
   ],
 
@@ -58,5 +115,8 @@ module.exports = {
     },
     open: true,
     port: 3000,
+    hot: true, 
+     // 파일 변경 감지
+    watchFiles: ['src/**/*'], 
   },
 };
